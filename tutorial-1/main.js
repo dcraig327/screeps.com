f3d644c2ffb907a17175b5c3d49b7180ce5a7c6c
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-11-04 05:18:40 -0700

    add starter code

---
+++ tutorial-1/main.js
@@ -0,0 +1,141 @@
+var roleHarvester = require("role.harvester");
+var roleUpgrader = require("role.upgrader");
+var roleBuilder = require("role.builder");
+
+function spawn() {
+  //
+}
+
+/*
+how many energy sources
+how many bots can harvest from each one
+what is the distance from each energy to spawn and ea. energy to controller
+ttl before death
+idle somewhere not in the way of creeps
+
+
+
+    STRUCTURE_SPAWN: "spawn",
+    STRUCTURE_EXTENSION: "extension",
+    STRUCTURE_ROAD: "road",
+    STRUCTURE_WALL: "constructedWall",
+    STRUCTURE_RAMPART: "rampart",
+    STRUCTURE_KEEPER_LAIR: "keeperLair",
+    STRUCTURE_PORTAL: "portal",
+    STRUCTURE_CONTROLLER: "controller",
+    STRUCTURE_LINK: "link",
+    STRUCTURE_STORAGE: "storage",
+    STRUCTURE_TOWER: "tower",
+    STRUCTURE_OBSERVER: "observer",
+    STRUCTURE_POWER_BANK: "powerBank",
+    STRUCTURE_POWER_SPAWN: "powerSpawn",
+    STRUCTURE_EXTRACTOR: "extractor",
+    STRUCTURE_LAB: "lab",
+    STRUCTURE_TERMINAL: "terminal",
+    STRUCTURE_CONTAINER: "container",
+    STRUCTURE_NUKER: "nuker",
+    STRUCTURE_FACTORY: "factory",
+    STRUCTURE_INVADER_CORE: "invaderCore",
+
+*/
+
+module.exports.loop = function () {
+  for (var name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory:", name);
+    }
+  }
+
+  for (let name in Game.rooms) {
+    let room = Game.rooms[name];
+    console.log(room.find(FIND_SOURCES));
+    //console.log(room.structure.structureType);
+  }
+
+  var harvesters = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "harvester"
+  );
+
+  var upgraders = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "upgrader"
+  );
+
+  var builders = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "builder"
+  );
+
+  console.log(
+    `Harvesters-Upgraders-Builders: ${harvesters.length}-${upgraders.length}-${builders.length}`
+  );
+
+  if (harvesters.length < 1) {
+    var newName = "Harvester" + Game.time;
+    console.log("Spawning new harvester: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: { role: "harvester" },
+    });
+  } else if (upgraders.length < 2) {
+    var newName = "Upgrader" + Game.time;
+    console.log("Spawning new upgrader: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: { role: "upgrader" },
+    });
+  } else if (builders.length < 1) {
+    var newName = "Builder" + Game.time;
+    console.log("Spawning new builder: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: { role: "builder" },
+    });
+  }
+
+  if (Game.spawns["Spawn1"].spawning) {
+    var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
+    Game.spawns["Spawn1"].room.visual.text(
+      "🛠️" + spawningCreep.memory.role,
+      Game.spawns["Spawn1"].pos.x + 1,
+      Game.spawns["Spawn1"].pos.y,
+      { align: "left", opacity: 0.8 }
+    );
+  }
+
+  var tower = Game.getObjectById("TOWER_ID");
+  if (tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(
+      FIND_STRUCTURES,
+      {
+        filter: (structure) => structure.hits < structure.hitsMax,
+      }
+    );
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
+
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == "harvester") {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role == "upgrader") {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == "builder") {
+      roleBuilder.run(creep);
+    }
+  }
+
+  // for (var name in Game.rooms) {
+  //   console.log(
+  //     'Room "' + name + '" has ' + Game.rooms[name].energyAvailable + " energy"
+  //   );
+  // }
+};
